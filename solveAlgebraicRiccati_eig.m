3085003d68d26302ef133f20c1ca0c2dba78f505
Sylvain Calinon <sylvain.calinon@gmail.com>
2014-07-30 17:25:33 +0200

    Adding ARE with Schur or Eig

--- solveAlgebraicRiccati_eig.m
+++ solveAlgebraicRiccati_eig.m
@@ -1,5 +1,6 @@
 function X = solveAlgebraicRiccati_eig(A, G, Q)
 %Solves the algebraic Riccati equation of the form A'X+XA'-XGX+Q=0, where X is symmetric with eigendecomposition.
+%Danilo Bruno, 2014
 
 n = size(A,1);
 Z = [A -G; -Q -A'];
